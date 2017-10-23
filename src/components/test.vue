367a29b33871bd1219c443913a60de5b84274057
Sergey Safronov <ssafronov@heliostech.fr>
2017-10-23 17:11:15 +0100

    start vue

---
+++ src/components/test.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="test">
+    <span v-html="title"></span>
+    <table>
+      <tr>
+        <td>{{ user.firstName }}</td>
+        <td v-text="user.lastName"></td>
+      </tr>
+    </table>
+    <p v-if="showName">It's working!!!</p>
+    <p v-else="showName">NO, it doesn't</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data(){
+    return{
+      title: '<h1>Hello<h1>',
+      user:{
+        firstName: 'Sergey',
+        lastName: 'Safronov'
+      },
+      showName: false
+   }
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
